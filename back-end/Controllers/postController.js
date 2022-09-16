e2933224f4aab0d525e759c565c30f1e0c1f7b70
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-09-15 19:08:46 -0700

    Show/Create/Show
    Adding Show/Craete/Show1 post.

--- back-end/Controllers/postController.js
+++ back-end/Controllers/postController.js
@@ -1,16 +1,75 @@
 const express = require('express');
 const posts = express.Router();
 const {
-    getAllPosts,
-} = require ('../Queries/posts');
+  getAllPosts,
+  getPost,
+  createPost,
+  deletePost,
+} = require('../Queries/posts');
 
 posts.get('/', async (req, res) => {
-    const allPosts = await getAllPosts();
-    if (allPosts[0]) {
-        res.status(200).json(allPosts);
-    }else {
-        res.status(500).json ({error: 'server error'})
-    }
+  const allPosts = await getAllPosts();
+  if (allPosts[0]) {
+    res.status(200).json(allPosts);
+  } else {
+    res.status(500).json({ error: 'server error' });
+  }
+});
+
+posts.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const post = await getPost(id);
+
+  if (post.id) {
+    res.status(200).json({
+      success: true,
+      payload: post,
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      payload: 'No valid post exists.',
+    });
+  }
+});
+
+//   CREATE
+posts.post('/', async (req, res) => {
+  const { body } = req;
+
+  const createdPost = await createPost(body);
+
+  if (createdPost.id) {
+    res.status(200).json({
+      success: true,
+      payload: {
+        id: createdPost.id,
+        name: createdPost.name,
+        image: createdPost.image,
+        caption: createdPost.caption,
+      },
+    });
+    return;
+  }
+  res.status(500).json({ error: 'Could not create post.' });
+});
+
+// DELETE
+posts.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedPost = await deletePost(id);
+  if (deletedPost.id) {
+    res.status(200).json({
+      success: true,
+      payload: deletedPost,
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      payload: { id: undefined },
+    });
+  }
 });
 
 module.exports = posts;
--- back-end/Queries/posts.js
+++ back-end/Queries/posts.js
@@ -1,17 +1,50 @@
-const db = require('../db/dbConfig')
+const { default: PostIndex } = require('../../front-end/src/Pages/Index');
+const posts = require('../Controllers/postController');
+const db = require('../db/dbConfig');
 
 // INDEX
 const getAllPosts = async () => {
+  try {
+    const allPosts = await db.any('SELECT * FROM posts');
+    return allPosts;
+  } catch (err) {
+    return err;
+  }
+};
+
+const getPost = async (id) => {
+  try {
+    return await db.one('SELECT * FROM posts WHERE id=$1', id);
+  } catch (err) {
+    return err;
+  }
+};
+
+const createPost = async (post) => {
     try {
-        const allPosts = await db.any('SELECT * FROM posts');
-        return allPosts
+        return await db.one (
+           ' INSERT INTO posts (name, image, caption) VALUES ($1, $2, $3) RETURNING *'
+           [
+            post.name,
+            post.image,
+            post.caption
+           ]
+        );
     }catch (err) {
         return err
     }
-};
-
-const getPost
-
-module.exports = {
-    getAllPosts,
 }
+
+const deletePost = async ( id) => {
+    try{
+        return await db.one("DELETE FROM posts WHERE id=$1 RETURNING *", id)
+    }catch (err){
+return err
+    }
+}
+module.exports = {
+  getAllPosts,
+  getPost,
+  createPost,
+  deletePost
+};
